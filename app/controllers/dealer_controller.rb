d7d72ecba9dc53503d28dbd3d0365854b366cd16
Akash Badshah <akashbad@mit.edu>
2012-10-09 13:31:01 -0400

    Added the Wikipedia Gem and implemented the functionality

--- app/controllers/dealer_controller.rb
+++ app/controllers/dealer_controller.rb
@@ -1,7 +1,10 @@
 class DealerController < ApplicationController
+	include DealerHelper
 	def home
 	end
 
 	def search
+		@result = how_big_a_deal_is params[:term]
+		# @result = "Hooray"
 	end
 end
--- app/helpers/dealer_helper.rb
+++ app/helpers/dealer_helper.rb
@@ -1,2 +1,26 @@
 module DealerHelper
+	require 'wikipedia'
+
+	def how_big_a_deal_is(term)
+		page = Wikipedia.find(term)
+		term = page.title
+		unless page.content
+			return "#{term} isn't a big deal at all"
+		end
+		length = page.content.length
+		case length
+		when 0..5000
+			return "#{term} is not that big a deal"
+		when 5000..10000
+			return "#{term} is sort of a big deal"
+
+		when 10000..50000
+			return "#{term} is a pretty big deal"
+
+		when 50000.100000
+			return "#{term} is a really big deal"
+		else
+			return "#{term} is a huge deal"
+		end
+	end
 end
