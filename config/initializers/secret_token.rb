5a0b6752508af9943d835625db5416dbbb40e309
Akash Badshah <akashbad@mit.edu>
2012-10-09 11:28:10 -0400

    added rails app and Dealer controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HowBigADeal::Application.config.secret_token = '7b26e88df0a1a081f442c32e1098ebbe5f6452919fd0af7ce5f824664f9e672a2140897e57ae1f2a3d35327046d0cc943f994721ca898fa85adc555902da9ddd'
---
+++ test/functional/dealer_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class DealerControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
